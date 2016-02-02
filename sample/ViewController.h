af9f98f20c831a09a485d5591767f917631eb888
nagarajanat <nagarajanat@zsl.com>
2016-02-02 12:55:14 +0530

    Initial Commit

---
+++ sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sample
+//
+//  Created by zylog on 02/02/16.
+//  Copyright © 2016 zylog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sample
+//
+//  Created by zylog on 02/02/16.
+//  Copyright © 2016 zylog. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
